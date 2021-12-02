4de9261e38380c209197105cd92e376cbff7fbd2
Atharva Deshpande <adrooney322@gmail.com>
2021-12-02 11:13:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js")
+const client = new Discord.Client();
+
+client.on('ready',async()=>{
+  console.log(`Bot has started in ${client.guilds.cache.size} servers with ${client.users.cache.size} members and ${client.channels.cache.size} channels`)
+})
+
+client.on('message',async(message)=>{
+    if(message.channel.id === '900867004351803442'){
+        message.member.setNickname(message.content).then(message.react('809086244130521109'));
+    }
+})
+client.login(process.env.BOT_TOKEN)
